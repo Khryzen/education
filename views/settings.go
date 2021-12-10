1f39c06f91c8fda8c2d0f6135415239bfe3e2dbb
unknown <mdeguzman.cpe@gmail.com>
2021-12-10 21:17:32 +0800

    ADD: User Settings UI

--- views/settings.go
+++ views/settings.go
@@ -6,6 +6,7 @@ import (
 
 func ProfileSettings(w http.ResponseWriter, r *http.Request) map[string]interface{} {
 	context := map[string]interface{}{}
+	// userprofile := uadmin.User{}
 
 	return context
 }
